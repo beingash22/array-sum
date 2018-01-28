7d7cd5dfc5efcf630feeb307afe9bca15632bc7f
beingash22 <ashrockzap@gmail.com>
2018-01-28 18:44:48 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main ()
+{
+int a[1000], n, k, i;
+scanf ("%d%d", n ,k);
+
+if (n>0 && k>0)
+{
+  for (i=0; i<n; i++)
+    scanf ("%d", &a[i])
+    
+  for (i=0; i<k; i++)
+    sum=sum+a[i];
+}
+else
+  printf ("invalid input");
+}
+  
