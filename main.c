ddff4bbe200f9497833649d45d696128fe526675
beingash22 <ashrockzap@gmail.com>
2018-01-28 18:47:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 
 void main ()
 {
-int a[1000], n, k, i, sum=0;
-scanf ("%d%d", n ,k);
+  int a[1000], n, k, i, sum=0;
+  scanf ("%d%d", n ,k);
 
-if (n>0 && k>0)
-{
-  for (i=0; i<n; i++)
-    scanf ("%d", &a[i])
-    
-  for (i=0; i<k; i++)
-    sum=sum+a[i];
-}
-else
-  printf ("invalid input");
+  if (n>0 && k>0)
+  {
+    for (i=0; i<n; i++)
+      scanf ("%d", &a[i])
+
+    for (i=0; i<k; i++)
+      sum=sum+a[i];
+    printf ("%d", sum);  
+  }
+  else
+    printf ("invalid input");
 }
   
